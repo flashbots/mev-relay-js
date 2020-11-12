0374e8fff0bfc7bde5253bb0ee281dd4450fd283
Jason Paryani <github@jparyani.com>
2020-11-11 17:40:27 -0800

    Add initial version of relay

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mev-relay
+
+## miner/proxy.js
+
+A simple example of a reverse proxy that a miner can run to expose just the eth_sendBundle JSON-RPC method
---
+++ server/main.js
@@ -0,0 +1,88 @@
+// A simple server that proxies only specific methods to an Ethereum JSON-RPC
+const express = require('express')
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
+const _ = require('lodash')
+
+const ALLOWED_METHODS = ['eth_sendBundle']
+
+function help() {
+  console.log('node ./server/main.js minerUrlS [PORT]')
+}
+
+function validPort(port) {
+  if (isNaN(port) || port < 0 || port > 65535) {
+    return false
+  }
+
+  return true
+}
+
+if (_.includes(process.argv, '-h') || _.includes(process.argv, '--help')) {
+  help()
+  process.exit(0)
+}
+
+const MINERS = _.split(process.argv[2], ',')
+if (MINERS.length === 0) {
+  console.error('no valid miner urls provided')
+  help()
+  process.exit(1)
+}
+
+const PORT = parseInt(_.get(process.argv, '[3]', '18545'))
+
+if (!validPort(PORT)) {
+  console.error(`invalid port specified for PORT: ${PORT}`)
+  process.exit(1)
+}
+
+const app = express()
+app.use(bodyParser.json())
+
+app.use(async (req, res) => {
+  if (!req.body) {
+    res.writeHead(400)
+    res.end('invalid json body')
+    return
+  }
+  if (!req.body.method) {
+    res.writeHead(400)
+    res.end('missing method')
+    return
+  }
+  if (!_.includes(ALLOWED_METHODS, req.body.method)) {
+    res.writeHead(400)
+    res.end(`invalid method, only ${ALLOWED_METHODS} supported, you provided: ${req.body.method}`)
+    return
+  }
+
+  const requests = []
+  MINERS.forEach((minerUrl) => {
+    try {
+      requests.push(
+        fetch(`${minerUrl}`, {
+          method: 'post',
+          body: JSON.stringify(req.body),
+          headers: { 'Content-Type': 'application/json' }
+        })
+      )
+    } catch (error) {
+      console.error('Error calling miner', minerUrl, error)
+    }
+  })
+
+  const responses = Promise.all(requests)
+
+  responses.forEach((response, i) => {
+    if (!response.ok) {
+      console.error(`http error calling miner ${MINERS[i]} with status ${response.status}`)
+    }
+  })
+
+  res.end('TODO')
+})
+
+app.listen(PORT, () => {
+  console.log(`relay listening at ${PORT}`)
+})

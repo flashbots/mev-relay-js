e876964b67822ba4e1af830017df30240996a6a4
Jason Paryani <github@jparyani.com>
2021-02-12 10:37:34 -0800

    Add blacklist to relay

--- server/bundle.js
+++ server/bundle.js
@@ -1,18 +1,28 @@
-const { decode } = require('@ethersproject/rlp')
-const ethers = require('ethers')
+const { Transaction } = require('@ethereumjs/tx')
+const _ = require('lodash')
 
-function decodeTxGasLimit(rawTx) {
-  const decodedTx = decode(rawTx)
+const BLACKLIST = [
+  // OFAC banned addresses
+  '0x8576acc5c05d6ce88f4e49bf65bdf0c62f91353c',
+  '0xd882cfc20f52f2599d84b8e8d58c7fb62cfe344b',
+  '0x901bb9583b24d97e995513c6778dc6888ab6870e',
+  '0xa7e5d5a720f06526557c513402f2e6b5fa20b00',
+  '0x7f367cc41522ce07553e823bf3be79a889debe1b'
+]
 
-  return ethers.BigNumber.from(decodedTx[2])
+function checkBlacklistTx(rawTx) {
+  const tx = Transaction.fromRlpSerializedTx(rawTx)
+
+  return _.includes(BLACKLIST, tx.to.toString()) || _.includes(BLACKLIST, tx.getSenderAddress().toString())
 }
-function sumBundleGasLimit(bundle) {
-  let sum = ethers.BigNumber.from(0)
+function checkBlacklist(bundle) {
   bundle.forEach((tx) => {
-    sum = sum.add(decodeTxGasLimit(tx))
+    if (checkBlacklistTx(tx)) {
+      return true
+    }
   })
 
-  return sum.toNumber()
+  return false
 }
 
-module.exports = { sumBundleGasLimit }
+module.exports = { checkBlacklist }

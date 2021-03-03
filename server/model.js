4379736007b95e7818b16a9baaa0d69fb457a7f3
Jason Paryani <github@jparyani.com>
2021-03-03 14:44:01 -0800

    Add back in old auth header
    Also remove global rate limit

--- server/model.js
+++ server/model.js
@@ -1,11 +1,34 @@
 const dynamoose = require('dynamoose')
+const crypto = require('crypto')
+const util = require('util')
+const _ = require('lodash')
+
+const pbkdf2 = util.promisify(crypto.pbkdf2)
 
 const Users = dynamoose.model(
-  'RelayUsersV2',
+  'RelayUsers',
   new dynamoose.Schema({
-    address: { type: String, index: { global: true } },
-    name: { type: String, index: { global: true } }
+    email: String,
+    keyID: { type: String, index: { global: true } },
+    hashedSecretKey: { type: String, index: { global: true } },
+    salt: String
   })
 )
 
-module.exports = { Users }
+function generateSalt() {
+  return crypto.randomBytes(32).toString('base64')
+}
+
+function generateKeyID() {
+  return _.trimEnd(crypto.randomBytes(32).toString('base64'), '=')
+}
+
+function generateSecretKey() {
+  return _.trimEnd(crypto.randomBytes(64).toString('base64'), '=')
+}
+
+async function hashPass(pass, salt) {
+  return (await pbkdf2(pass, salt, 1000, 64, 'sha512')).toString('hex')
+}
+
+module.exports = { Users, generateSalt, generateKeyID, generateSecretKey, hashPass }
--- server/scripts/dbhelper.js
+++ server/scripts/dbhelper.js
@@ -1,4 +1,4 @@
-const { Users } = require('../model')
+const { Users, generateSalt, generateKeyID, generateSecretKey, hashPass } = require('../model')
 
 async function main() {
   if (process.argv.length < 3) {
@@ -7,20 +7,33 @@ async function main() {
   }
 
   const command = process.argv[2]
-  if (command === 'scan') {
+  if (command === 'create') {
+    const salt = generateSalt()
+    const secretKey = generateSecretKey()
+    const keyID = generateKeyID()
+    const user = new Users({
+      email: process.argv[3],
+      keyID,
+      hashedSecretKey: await hashPass(secretKey, salt),
+      salt: salt
+    })
+    await user.save()
+    console.log(`FLASHBOTS_KEY_ID=${keyID}`)
+    console.log(`FLASHBOTS_SECRET=${secretKey}`)
+  } else if (command === 'scan') {
     console.log((await Users.scan().all().exec()).toJSON())
   } else if (command === 'dump') {
     const users = await Users.scan().all().exec()
-    console.log('name,address')
+    console.log('email,shortKey')
     users.forEach((element) => {
-      console.log(`${element.name},${element.address}`)
+      console.log(`${element.email},${element.keyID.slice(0, 8)}`)
     })
-  } else if (command === 'getByName') {
-    console.log(await Users.query('name').eq(process.argv[3]).exec())
-  } else if (command === 'getByAddress') {
-    console.log(await Users.query('address').eq(process.argv[3]).exec())
-  } else if (command === 'getByShortAddress') {
-    console.log(await Users.scan('address').beginsWith(process.argv[3]).exec())
+  } else if (command === 'getByEmail') {
+    console.log(await Users.query('email').eq(process.argv[3]).exec())
+  } else if (command === 'getByKeyID') {
+    console.log(await Users.query('keyID').eq(process.argv[3]).exec())
+  } else if (command === 'getByShortKeyID') {
+    console.log(await Users.scan('keyID').beginsWith(process.argv[3]).exec())
   } else {
     console.error('unknown command')
     process.exit(1)

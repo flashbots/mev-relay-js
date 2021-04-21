cd5b5e3957101d37cf1735be88035950b1947eab
Jason Paryani <github@jparyani.com>
2021-04-21 13:39:12 -0700

    Update OFAC ban list to correct address
    Fixes #41

--- server/bundle.js
+++ server/bundle.js
@@ -7,7 +7,8 @@ const BLACKLIST = [
   '0x8576acc5c05d6ce88f4e49bf65bdf0c62f91353c',
   '0xd882cfc20f52f2599d84b8e8d58c7fb62cfe344b',
   '0x901bb9583b24d97e995513c6778dc6888ab6870e',
-  '0xa7e5d5a720f06526557c513402f2e6b5fa20b00',
+  '0xa7e5d5a720f06526557c513402f2e6b5fa20b00', // this is an invalid address, but is what's listed in the OFAC ban list
+  '0xa7e5d5a720f06526557c513402f2e6b5fa20b008', // the actual valid address
   '0x7f367cc41522ce07553e823bf3be79a889debe1b'
 ]
 

76c21d55bf7312c702488690913634bc167f1fdb
Micah Zoltu <micah@zoltu.net>
2021-04-21 15:43:38 +0800

    Removes blacklist.
    Getting away from this stuff is a big part of the entire point of Ethereum and censorship resistant finance.  For starters, banning these addresses does absolutely nothing to stop them from being used.  Secondly, even if it did, the whole point of censorship resistance is that we should be moving toward a brighter future free from government censorship of financial freedom.
    
    This blacklist goes against the entire ethos of Ethereum and is an anathema on the project and should be removed promptly.

--- server/bundle.js
+++ server/bundle.js
@@ -2,14 +2,7 @@ const { Transaction } = require('@ethereumjs/tx')
 const Common = require('@ethereumjs/common').default
 const _ = require('lodash')
 
-const BLACKLIST = [
-  // OFAC banned addresses
-  '0x8576acc5c05d6ce88f4e49bf65bdf0c62f91353c',
-  '0xd882cfc20f52f2599d84b8e8d58c7fb62cfe344b',
-  '0x901bb9583b24d97e995513c6778dc6888ab6870e',
-  '0xa7e5d5a720f06526557c513402f2e6b5fa20b00',
-  '0x7f367cc41522ce07553e823bf3be79a889debe1b'
-]
+const BLACKLIST = []
 
 const MAX_DISTINCT_TO = 2
 
